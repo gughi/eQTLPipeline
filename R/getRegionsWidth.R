c036c6ed3bb62e29188a4df5666e2b5a92ddf692
gughi <manuelsebastian.guelfi@gmail.com>
2015-09-23 10:39:29 +0100

    Changed the function getRegionsWidth to also calculate intornic width

--- R/getRegionsWidth.R
+++ R/getRegionsWidth.R
@@ -1,11 +1,14 @@
 getRegionsWidth <-
-function(geneID,exonsdef)
+function(geneID,exonsdef,intronic=FALSE)
 {
   regionsGene <- exonsdef[which(exonsdef$Ensembl.Gene.ID %in% geneID),]
   ## remove duplicates
   regionsGene <- unique(regionsGene)
   ## regionsGene: matrix with the structure of the exons of a gene
-  regionsGene <- defExonicRegions(regionsGene)
+  if(!intronic)
+  {
+    regionsGene <- defExonicRegions(regionsGene)
+  }
   ## write the GC content
   sums <- regionsGene$Exon.Chr.End..bp. -regionsGene$Exon.Chr.Start..bp.
   return(c(paste(geneID),sum(sums)))
