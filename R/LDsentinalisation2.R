67a6b2bec70087dfdd90d9940002229ec31b0c45
gughi <manuelsebastian.guelfi@gmail.com>
2015-09-28 13:19:19 +0100

    fixed bugs

--- R/LDsentinalisation2.R
+++ R/LDsentinalisation2.R
@@ -8,7 +8,7 @@ LDsentinalisation2 <-
     
     ## if exons
     geneID <- unlist(lapply(strsplit(as.character(regID),":"),function(x){x[1]}))
-    geneID <- unlist(sapply(strsplit(as.character(regID),"+",fixed=T),function(x){x[1]}))
+    geneID <- unlist(sapply(strsplit(as.character(geneID),"+",fixed=T),function(x){x[1]}))
 
     ## initialise my.cov0
     fn.rda <- paste0(snpLocation, geneID, ".rda")
--- sentiExons.R
+++ sentiExons.R
@@ -16,11 +16,11 @@ getDoParWorkers()
 ## path where get the unsentinalised eQTLs
 pathUnsentinalised <- "data/results/genic/exons/resMatrixEQTL/PUTM/"
 
-regions <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
+exons <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
 
 
 ## load the residual corrected expression
-load("data/expr/normalisedCounts/genic/geneIntronic/resids.PUTM.rda")
+load("data/expr/normalisedCounts/genic/exons/resids.PUTM.rda")
 
 load("data/general/sampleInfo.rda")
 IDs <- sampleInfo[which(sampleInfo$A.CEL_file %in% as.character(rownames(resids))),"U.SD_No"]
@@ -40,8 +40,8 @@ my.covTMP <- read.table.rows(paste0("/home/guelfi/plinkOutput/eigenvec"), keepRo
 
 
 Sys.time()
-foreach(i=1:nrow(regions))%dopar%LDsentinalisation2(resids=resids,
-                                                    regID=regions[i,1],
+foreach(i=1:nrow(exons))%dopar%LDsentinalisation2(resids=resids,
+                                                    regID=exons[i,1],
                                                     pathFinalSentinalised=pathFinalSentinalised,
                                                     pathUnsentinalised=pathUnsentinalised,
                                                     FDRthr=0.10,
