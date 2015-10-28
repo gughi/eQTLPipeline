d91baf27105fb195be382bcd9019cdd8d7c52ac7
gughi <m.guelfi@ucl.ac.uk>
2015-10-28 15:28:58 +0000

    minor updates

--- parRunCiseQTLExExJunc.R
+++ parRunCiseQTLExExJunc.R
@@ -9,19 +9,23 @@ library(doParallel)
 library(foreach)
 load_all()
 
+
+# Caprica
+setwd("/home/seb/projectsR/eQTLPipeline/")
 ## load the annotation for the exon-exon junctions
 load("data/expr/rawCounts/genic/fullExExJun.rda")
 
-setwd("/home/guelfi/eQTLPipeline/")
-cl <- makeCluster(7)
-clusterExport(cl,c("runCisEQTLExons","read.table.rows"))
+# Apollo
+# setwd("/home/guelfi/eQTLPipeline/")
+cl <- makeCluster(20)
+clusterExport(cl,c("runCisEQTLExExJun","read.table.rows"))
 registerDoParallel(cl)
 getDoParWorkers()
 
 ## load the residual corrected expression
-load("data/expr/normalisedCounts/genic/exons/resids.PUTM.rda")
+load("data/expr/normalisedCounts/genic/exonExonJunc/resids.PUTM.rda")
 ## load the information about tissue
-load("data/expr/normalisedCounts/genic/exons/RPKM.cqn.PUTM")
+load("data/expr/normalisedCounts/genic/exonExonJunc/RPKM.cqn.PUTM")
 ## remove the object we don't need
 rm(covs,RPKM.cqn)
 
@@ -32,19 +36,43 @@ IDs <- gsub("/","_",IDs)
 rownames(resids) <- IDs
 rm(IDs)
 
-snpLocation <- "/home/guelfi/eQTL/snps/byGene/"
+## Apollo
+## snpLocation <- "/home/guelfi/eQTL/snps/byGene/"
+## Caprica
+snpLocation <- "/home/seb/eQTL/snps/byGene/"
 
-outputFolder <- "/home/guelfi/eQTLPipeline/data/results/genic/exons/resMatrixEQTL/PUTM/"
-fullResults <- "/home/guelfi/eQTLPipeline/data/results/genic/exons/fullResults/PUTM/"
+# Apollo
+# outputFolder <- "/home/guelfi/eQTLPipeline/data/results/genic/exonExonJunc/resMatrixEQTL/PUTM/"
+# fullResults <- "/home/guelfi/eQTLPipeline/data/results/genic/exonExonJunc/fullResults/PUTM/"
 
-my.covTMP <- read.table.rows(paste0("/home/guelfi/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
+# Caprica
+outputFolder <- "/home/seb/projectsR/eQTLPipeline/data/results/genic/exonExonJunc/resMatrixEQTL/PUTM/"
+fullResults <- "/home/seb/projectsR/eQTLPipeline/data/results/genic/exonExonJunc/fullResults/PUTM/"
+
+# Apollo
+# my.covTMP <- read.table.rows(paste0("/home/guelfi/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
+# Caprica
+my.covTMP <- read.table.rows(paste0("/home/seb/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
 
 Sys.time()
-foreach(i=1:ncol(resids))%dopar%runCisEQTLExons(i=i,resids=resids,
+
+foreach(i=1:1)%dopar%runCisEQTLExExJun(i=i,resids=resids,
+                                                     mapExon=mapExon,
                                                      snpLocation=snpLocation,
                                                      outputFolder=outputFolder,
                                                      my.covTMP=my.covTMP,
                                                      fullResults=fullResults)
+
+
+foreach(i=1:1)%dopar%print(i)
+foreach(i=1:2)%dopar%print(i)
+
+runCisEQTLExExJun(i=4,resids=resids,
+                  mapExon=mapExon,
+                  snpLocation=snpLocation,
+                  outputFolder=outputFolder,
+                  my.covTMP=my.covTMP,
+                  fullResults=fullResults)
 
 Sys.time()
 stopCluster(cl)
