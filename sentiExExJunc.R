6e5745713182de6f10e842280abc966e06eac7cb
gughi <m.guelfi@ucl.ac.uk>
2015-11-04 19:31:35 +0000

    Added code for annotate the exon-exon junctions

--- sentiExExJunc.R
+++ sentiExExJunc.R
@@ -8,64 +8,75 @@ library(doParallel)
 library(foreach)
 load_all()
 
-setwd("/home/guelfi/eQTLPipeline/")
-# cl <- makeCluster(7)
-# clusterExport(cl,c("LDsentinalisation2","read.table.rows"))
-# registerDoParallel(cl)
+# Caprica
+setwd("/home/seb/projectsR/eQTLPipeline/")
+# Apollo
+# setwd("/home/guelfi/eQTLPipeline/")
+
+cl <- makeCluster(20)
+clusterExport(cl,c("LDsentExonExonJunc","read.table.rows"))
+registerDoParallel(cl)
+
 # getDoParWorkers()
 # ## path where get the unsentinalised eQTLs
-# pathUnsentinalised <- "data/results/genic/exons/resMatrixEQTL/PUTM/"
-# 
-# exons <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
-# 
-# 
-# ## load the residual corrected expression
-# load("data/expr/normalisedCounts/genic/exons/resids.PUTM.rda")
-# 
-# load("data/general/sampleInfo.rda")
-# IDs <- sampleInfo[which(sampleInfo$A.CEL_file %in% as.character(rownames(resids))),"U.SD_No"]
-# IDs <- gsub("/","_",IDs)
-# rownames(resids) <- IDs
-# rm(indID,IDs)
-# 
-# pathFinalSentinalised <-"data/results/genic/exons/resMatrixEQTL/sentinalised/"
-# dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
-# pathFinalSentinalised <-"data/results/genic/exons/resMatrixEQTL/sentinalised/PUTM/"
-# dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
-# 
-# dir.create(file.path("tmp", "PUTM"),showWarnings=FALSE)
-# 
-# # load the genetic PCs
-# my.covTMP <- read.table.rows(paste0("/home/guelfi/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
-# 
-# 
-# Sys.time()
-# foreach(i=1:nrow(exons))%dopar%LDsentinalisation2(resids=resids,
-#                                                     regID=exons[i,1],
-#                                                     pathFinalSentinalised=pathFinalSentinalised,
-#                                                     pathUnsentinalised=pathUnsentinalised,
-#                                                     FDRthr=0.10,
-#                                                     my.covTMP=my.covTMP,
-#                                                     snpLocation="/home/guelfi/eQTL/snps/byGene/",
-#                                                     tmpFolder="tmp/")
-# Sys.time()
-# stopCluster(cl)
+pathUnsentinalised <- "data/results/genic/exonExonJunc/resMatrixEQTL/PUTM/"
+junctions <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
+
+## load the residual corrected expression
+load("data/expr/normalisedCounts/genic/exonExonJunc/resids.PUTM.rda")
+ 
+load("data/general/sampleInfo.rda")
+IDs <- sampleInfo[which(sampleInfo$A.CEL_file %in% as.character(rownames(resids))),"U.SD_No"]
+IDs <- gsub("/","_",IDs)
+rownames(resids) <- IDs
+rm(indID,IDs)
+ 
+pathFinalSentinalised <-"data/results/genic/exonExonJunc/resMatrixEQTL/sentinalised/"
+dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
+pathFinalSentinalised <-"data/results/genic/exonExonJunc/resMatrixEQTL/sentinalised/PUTM/"
+dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
+
+dir.create(file.path("tmp", "PUTM"),showWarnings=FALSE)
+ 
+# load the genetic PCs
+my.covTMP <- read.table.rows(paste0("/home/seb/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
+
+## load mapping file
+load("data/expr/rawCounts/genic/fullExExJun.rda")
+rm(map,expr)
+
+Sys.time()
+foreach(i=1:nrow(junctions))%dopar%LDsentExonExonJunc(resids=resids,
+                                                    regID=junctions[i,1],
+                                                    mapExon=mapExon,
+                                                    pathFinalSentinalised=pathFinalSentinalised,
+                                                    pathUnsentinalised=pathUnsentinalised,
+                                                    FDRthr=0.10,
+                                                    my.covTMP=my.covTMP,
+                                                    snpLocation="/home/seb/eQTL/snps/byGene/",
+                                                    tmpFolder="tmp/")
+Sys.time()
+stopCluster(cl)
 
 rm(list=ls())
 
 
-cl <- makeCluster(7)
-clusterExport(cl,c("LDsentinalisation2","read.table.rows"))
+# Caprica
+setwd("/home/seb/projectsR/eQTLPipeline/")
+# Apollo
+# setwd("/home/guelfi/eQTLPipeline/")
+
+cl <- makeCluster(20)
+clusterExport(cl,c("LDsentExonExonJunc","read.table.rows"))
 registerDoParallel(cl)
-getDoParWorkers()
-## path where get the unsentinalised eQTLs
-pathUnsentinalised <- "data/results/genic/exons/resMatrixEQTL/SNIG/"
 
-exons <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
-
+# getDoParWorkers()
+# ## path where get the unsentinalised eQTLs
+pathUnsentinalised <- "data/results/genic/exonExonJunc/resMatrixEQTL/SNIG/"
+junctions <- read.delim(pipe(paste0("ls ",pathUnsentinalised)),header=F)
 
 ## load the residual corrected expression
-load("data/expr/normalisedCounts/genic/exons/resids.SNIG.rda")
+load("data/expr/normalisedCounts/genic/exonExonJunc/resids.SNIG.rda")
 
 load("data/general/sampleInfo.rda")
 IDs <- sampleInfo[which(sampleInfo$A.CEL_file %in% as.character(rownames(resids))),"U.SD_No"]
@@ -73,29 +84,31 @@ IDs <- gsub("/","_",IDs)
 rownames(resids) <- IDs
 rm(indID,IDs)
 
-pathFinalSentinalised <-"data/results/genic/exons/resMatrixEQTL/sentinalised/"
+pathFinalSentinalised <-"data/results/genic/exonExonJunc/resMatrixEQTL/sentinalised/"
 dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
-pathFinalSentinalised <-"data/results/genic/exons/resMatrixEQTL/sentinalised/SNIG/"
+pathFinalSentinalised <-"data/results/genic/exonExonJunc/resMatrixEQTL/sentinalised/SNIG/"
 dir.create(file.path(pathFinalSentinalised),showWarnings=FALSE)
 
 dir.create(file.path("tmp", "SNIG"),showWarnings=FALSE)
 
 # load the genetic PCs
-my.covTMP <- read.table.rows(paste0("/home/guelfi/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
+my.covTMP <- read.table.rows(paste0("/home/seb/plinkOutput/eigenvec"), keepRows=rownames(resids), sep=" ",header=F)
 
+## load mapping file
+load("data/expr/rawCounts/genic/fullExExJun.rda")
+rm(map,expr)
 
 Sys.time()
-foreach(i=1:nrow(exons))%dopar%LDsentinalisation2(resids=resids,
-                                                  regID=exons[i,1],
-                                                  pathFinalSentinalised=pathFinalSentinalised,
-                                                  pathUnsentinalised=pathUnsentinalised,
-                                                  FDRthr=0.10,
-                                                  my.covTMP=my.covTMP,
-                                                  snpLocation="/home/guelfi/eQTL/snps/byGene/",
-                                                  tmpFolder="tmp/")
+foreach(i=1:nrow(junctions))%dopar%LDsentExonExonJunc(resids=resids,
+                                                      regID=junctions[i,1],
+                                                      mapExon=mapExon,
+                                                      pathFinalSentinalised=pathFinalSentinalised,
+                                                      pathUnsentinalised=pathUnsentinalised,
+                                                      FDRthr=0.10,
+                                                      my.covTMP=my.covTMP,
+                                                      snpLocation="/home/seb/eQTL/snps/byGene/",
+                                                      tmpFolder="tmp/")
 Sys.time()
 stopCluster(cl)
-
-
 
 
