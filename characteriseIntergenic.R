f60b1e5706f3c09778a98350d4d995a5cbc9b4c5
gughi <manuelsebastian.guelfi@gmail.com>
2016-02-19 20:48:42 +0000

    update the intergenic annotation

--- characteriseIntergenic.R
+++ characteriseIntergenic.R
@@ -1,4 +1,8 @@
 
+library(R.utils)
+path <- readWindowsShortcut("data.lnk", verbose=FALSE)
+setwd(dirname(path$networkPathname))
+rm(path)
 
 ## load teh eQTL table
 load("data/results/finaleQTLs/intergenic.Ann.PUTM.rda")
@@ -10,19 +14,9 @@ eQTLPUTM$exonJunc <- 0
 ## exonJuncCounts tells how many exon junctions has been seeying in total in all the samples
 eQTLPUTM$exonJuncCounts <- 0
 
-
-samples <- c("A653_001_CEL", "A653_012_CEL", "A653_031_CEL", "A653_048_CEL", "A653_056_CEL", "A653_057_CEL",
-             "A653_063_CEL","A653_066_CEL","A653_071_CEL","A653_098_CEL","A653_106_CEL","A653_107_CEL","A653_108_CEL",
-             "A653_122_CEL")
-
-for(i in 1:length(samples))
-{
-  eQTLPUTM <- getSummaryNovExon(eQTLPUTM,intNovelExonPath=paste0("data/junctions/PUTM/",samples[i],"_intergenicNovelExon.rda"))
-}
-
-table(eQTLPUTM$exonJunc)
-
-eQTLPUTM[which(eQTLPUTM$exonJunc>1),]
+samples <- list.files("data/junctions/PUTM/")
+samples <- unlist(lapply(strsplit(as.character(samples),"CEL_"),function(x){x[1]}))
+samples <- paste0(unique(samples),"CEL")
 
 
 ## with this we get a summary for the novel exon in our eQTLs
@@ -45,79 +39,120 @@ getSummaryNovExon <- function(eQTL,intNovelExonPath)
   }
   rm(intNovelExon)
   return(eQTL)
-
+  
 }
+
+
+
+
+for(i in 1:length(samples))
+{
+  eQTLPUTM <- getSummaryNovExon(eQTLPUTM,intNovelExonPath=paste0("data/junctions/PUTM/",samples[i],"_intergenicNovelExon.rda"))
+}
+
+table(eQTLPUTM$exonJunc>0)
+nrow(eQTLPUTM[which(eQTLPUTM$exonJunc>0),])
+
+hist(eQTLPUTM[which(eQTLPUTM$exonJunc>0),"exonJunc"],breaks=40,main="Novel exon identify by sample",xlab="samples")
+
+## example
+regInformation["DER591",]
+
+
+
+###########################
+#### Beta interaction #####
+###########################
+
+
 
 
 load("data/general/defIntergenic.rda")
 
-ExonExtention <- regInformation[which(as.numeric(as.character(regInformation$overlap)) == 1),]
-
 load("data/results/finaleQTLs/intergenic.Ann.PUTM.rda")
 
-eQTLPUTM <- eQTLPUTM[which(eQTLPUTM$myFDR <=0.05),1:7]
+eQTLs <- eQTLPUTM[which(eQTLPUTM$myFDR <=0.05),1:7]
 
 regInformation <- regInformation[as.character(eQTLPUTM$gene),]
 
-regInformation <- regInformation[which(as.numeric(as.character(regInformation$uniquePortion))>0), ]
-
-
-nrow(regInformation[which(as.numeric(as.character(regInformation$distance))<0), ])
-
-
 load("data/expr/normalisedCounts/genic/geneExons/resids.PUTM.rda")
 exprExonic <- resids
-load("data/expr/normalisedCounts/intergenic/RPKM.cqn.PUTM")
-exprIntronic <- resids
-rm(resids,listNonOve)
+load("data/expr/normalisedCounts/intergenic/resids.PUTM.rda")
+exprIntergenic <- resids
+rm(resids,eQTLPUTM)
 
-eQTLs <- eQTLs[which(as.character(eQTLs$gene) %in% as.character(colnames(exprIntronic))),]
-eQTLs <- eQTLs[which(as.character(eQTLs$gene) %in% as.character(colnames(exprExonic))),]
+head(regInformation)
+eQTLs <-  cbind(eQTLs,regInformation[as.character(eQTLs$gene),])
+colnames(eQTLs)[2] <- "region"
+head(eQTLs)
 
-## remove eQTLs that are duplicated
-eQTLs <- eQTLs[-which(duplicated(eQTLs[,1:2])),]
+## we now collect the information for each gene
+
+
+eQTLExonic <- NULL
+for(i in 1:nrow(eQTLs))
+{
+  if(is.element(as.character(eQTLs$gene[i]),colnames(exprExonic)))
+  {
+    tmp <- read.delim(paste0("data/results/genic/geneExons/fullResults/PUTM/",as.character(eQTLs$gene[i])),row.names = 1)
+    eQTLExonic <- rbind(eQTLExonic,tmp[as.character(eQTLs$snps[i]),c("ge.beta","ge.t.stat","ge.p.value","ge.FDR")])
+
+  }else{
+    eQTLExonic <- rbind(eQTLExonic,as.data.frame(t(as.matrix(c(beta=NA,t.stat=NA,p.value=NA,FDR=NA))),row.names=as.character(eQTLs$snps[i])))
+  }
+  rm(tmp)
+}
+rm(i)
+
 
 res <- NULL
 for (j in 1:nrow(eQTLs))
 {
   ## load the expression for each individual gene for exonic and intronic
-  exprE <- exprExonic[,as.character(eQTLs$gene[j])]
-  exprI <- exprIntronic[,as.character(eQTLs$gene[j])]
+  is.element(as.character(eQTLs$gene[i]),colnames(exprExonic))
+  {
+    exprE <- exprExonic[,as.character(eQTLs$gene[j])]
+    exprInt <- exprIntergenic[,as.character(eQTLs$region[j])]
   
-  ## add to the expression the tissue information or the exonic/intronic information
-  expr <- cbind(exprE,"Exonic")
-  exprtmp <- cbind(exprI,"Intronic")
-  expr <- rbind(expr,exprtmp)
-  rm(exprtmp,exprE,exprI)
+    ## add to the expression the tissue information or the exonic/intronic information
+    expr <- cbind(exprE,"Exonic")
+    exprtmp <- cbind(exprInt,"Intergenic")
+    expr <- rbind(expr,exprtmp)
+    rm(exprtmp,exprE,exprInt)
   
-  ## load the snps
-  load(paste0("/home/seb/eQTL/snps/byGene/",as.character(eQTLs$gene[j]),".rda"))
+    ## load the snps
+    load(paste0("/home/seb/eQTL/snps/byGene/",as.character(eQTLs$gene[j]),".rda"))
+    
+    ## load the samples information
+    load("data/general/sampleInfo.rda")
+    
+    
+    ## change the samples names and remove outlier
+    sampleInfo$U.SD_No <- gsub("/","_",sampleInfo$U.SD_No)
+    #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "95_40"),]
+    #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "09_50"),]
+    #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "034_07"),]
+    #   
+    rownames(sampleInfo) <- sampleInfo$A.CEL_file
+    selSamples <- sampleInfo[as.character(rownames(expr)),1]
+    
+    ##snp0 <- markers[as.character(eQTLs$snps[j]),unique(as.character(gsub("/","_",sampleInfo$U.SD_No)))]
+    
+    snp0 <- markers[as.character(eQTLs$snps[j]),selSamples]
+    ##snp0 <- round(snp0)
+    
+    ## markers[as.character(eQTLs$snps[j]),unique(as.character(gsub("/","_",sampleInfo$U.SD_No)))]
+    
+    ## rownames(sampleInfo) <- sampleInfo$A.CEL_file
+    
+    colnames(snp0) <- rownames(expr)
   
-  ## load the samples information
-  load("data/general/sampleInfo.rda")
+    f <- betaInteraction(expr,snp0)
+  }else
+  {
+    f <- NA
+  }
   
-  
-  ## change the samples names and remove outlier
-  sampleInfo$U.SD_No <- gsub("/","_",sampleInfo$U.SD_No)
-  #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "95_40"),]
-  #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "09_50"),]
-  #   sampleInfo <- sampleInfo[-which(sampleInfo$U.SD_No %in% "034_07"),]
-  #   
-  rownames(sampleInfo) <- sampleInfo$A.CEL_file
-  selSamples <- sampleInfo[as.character(rownames(expr)),1]
-  
-  ##snp0 <- markers[as.character(eQTLs$snps[j]),unique(as.character(gsub("/","_",sampleInfo$U.SD_No)))]
-  
-  snp0 <- markers[as.character(eQTLs$snps[j]),selSamples]
-  ##snp0 <- round(snp0)
-  
-  ## markers[as.character(eQTLs$snps[j]),unique(as.character(gsub("/","_",sampleInfo$U.SD_No)))]
-  
-  ## rownames(sampleInfo) <- sampleInfo$A.CEL_file
-  
-  colnames(snp0) <- rownames(expr)
-  
-  f <- betaInteraction(expr,snp0)
   ## collect summary statisctics from gene exonic
   cmd <- paste0("grep -w '",as.character(eQTLs$snps[j]),"' data/results/genic/geneExons/fullResults/PUTM/",as.character(eQTLs$gene[j]))
   statExonic <- read.delim(pipe(cmd),header=F)
